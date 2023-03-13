7349b9b2ff37e0c610607869e4617326e93ca122
Keunmo <9858momo@gmail.com>
2023-03-13 13:42:25 +0900

    add synthetic_shapes_torch

--- superpoint/datasets/base_dataset_torch.py
+++ superpoint/datasets/base_dataset_torch.py
@@ -3,8 +3,10 @@ from torchvision.datasets.vision import VisionDataset
 
 from superpoint.utils.tools import dict_update
 
+from typing import Callable, Optional, Any
+from PIL import Image
 
-class BaseDataset(metaclass=ABCMeta):
+class BaseDataset(VisionDataset):
     """Base model class.
 
     Arguments:
@@ -16,6 +18,40 @@ class BaseDataset(metaclass=ABCMeta):
         default_config: A dictionary of potential default configuration values (e.g. the
             size of the validation set).
     """
-    split_names = ['training', 'validation', 'test']
+    # def __init__(self, root: str, train: bool, transform: Optional[Callable] = None, target_transform: Optional[Callable] = None) -> None:
+    #     super(BaseDataset, self).__init__(root, transform=transform, target_transform=target_transform)
+    #     raise NotImplementedError
+    @abstractmethod
+    def _init_dataset(self, **config):
+        """Prepare the dataset for reading.
 
+        This method should configure the dataset for later fetching through `_get_data`,
+        such as downloading the data if it is not stored locally, or reading the list of
+        data files from disk. Ideally, especially in the case of large images, this
+        method shoudl NOT read all the dataset into memory, but rather prepare for faster
+        seubsequent fetching.
+
+        Arguments:
+            config: A configuration dictionary, given during the object instantiantion.
+
+        Returns:
+            An object subsequently passed to `_get_data`, e.g. a list of file paths and
+            set splits.
+        """
+        raise NotImplementedError
+
+    # make read init param from yaml config
+    def __init__(self, **config):
+        self.config = dict_update(getattr(self, 'default_config', {}), config)
+        self.root = self.config['root']
+        self.train = self.config['train']
+        self.transform = self.config['transform']
+        self.target_transform = self.config['target_transform']
+        self.dataset = self._init_dataset(**self.config)
+    
+    def __getitem__(self, index: int) -> Any:
+        raise NotImplementedError
+
+    def __len__(self) -> int:
+        raise NotImplementedError
     
